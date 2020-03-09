6449524d333d38d4a02de881b764b8291845eaf3
Paulo Gomes da Cruz Junior <paulushc@gmail.com>
2020-03-09 07:52:58 -0700

    Initial Commit

---
+++ src/main/java/io/github/paulushcgcj/CompanyHubApiApplication.java
@@ -0,0 +1,13 @@
+package io.github.paulushcgcj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompanyHubApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompanyHubApiApplication.class, args);
+	}
+
+}
